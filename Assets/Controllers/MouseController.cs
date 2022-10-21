36a3143bbc579915a3ed6e18d9f36a66198346af
Dvoryanoff <dvoryanoff@mail.ru>
2022-10-21 20:03:17 +0300

    visible drag-n-drop area

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour {
 
-    [SerializeField] private GameObject circleCursor;
+    [SerializeField] private GameObject circleCursorPrefab;
 
     private Vector3 lastFramePosition;
     private Vector3 dragStartPosition;
@@ -13,7 +13,7 @@ public class MouseController : MonoBehaviour {
         currentFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         currentFramePosition.z = 0;
 
-        UpdateCursor();
+        //UpdateCursor();
         UpdateDragging();
         UpdateMainCamera();
 
@@ -44,22 +44,33 @@ public class MouseController : MonoBehaviour {
             dragStartPosition = currentFramePosition;
         }
 
+        int startX = Mathf.FloorToInt(dragStartPosition.x);
+        int endX = Mathf.FloorToInt(currentFramePosition.x);
+        int startY = Mathf.FloorToInt(dragStartPosition.y);
+        int endY = Mathf.FloorToInt(currentFramePosition.y);
+
+        if (endX < startX) {
+            (startX, endX) = (endX, startX);
+        }
+
+        if (endY < startY) {
+            (startY, endY) = (endY, startY);
+        }
+
+        if (Input.GetMouseButton(0)) {
+            for (int x = startX; x <= endX; x++) {
+                for (int y = startY; y <= endY; y++) {
+                    Tile t = WorldController.Instance.World.GetTileAt(x, y);
+                    if (t != null) {
+                        Instantiate(circleCursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+            }
+        }
+
         // End Drag
 
         if (Input.GetMouseButtonUp(0)) {
-            int startX = Mathf.FloorToInt(dragStartPosition.x);
-            int endX = Mathf.FloorToInt(currentFramePosition.x);
-            int startY = Mathf.FloorToInt(dragStartPosition.y);
-            int endY = Mathf.FloorToInt(currentFramePosition.y);
-
-            if (endX < startX) {
-                (startX, endX) = (endX, startX);
-            }
-
-            if (endY < startY) {
-                (startY, endY) = (endY, startY);
-            }
-
             for (int x = startX; x <= endX; x++) {
                 for (int y = startY; y <= endY; y++) {
                     Tile t = WorldController.Instance.World.GetTileAt(x, y);
@@ -70,17 +81,16 @@ public class MouseController : MonoBehaviour {
             }
         }
     }
-
-    private void UpdateCursor() {
-        Tile tileUnderMouse = WorldController.Instance.GetTileAtWorldCoord(currentFramePosition);
-
-        if (tileUnderMouse != null) {
-            circleCursor.SetActive(true);
-
-            Vector3 cursorPosition = new Vector3(tileUnderMouse.X, tileUnderMouse.Y, 0);
-            circleCursor.transform.position = cursorPosition;
-        } else { circleCursor.SetActive(false); }
-    }
-
 }
+
+//private void UpdateCursor() {
+//    Tile tileUnderMouse = WorldController.Instance.GetTileAtWorldCoord(currentFramePosition);
+
+//    if (tileUnderMouse != null) {
+//        circleCursor.SetActive(true);
+
+//        Vector3 cursorPosition = new Vector3(tileUnderMouse.X, tileUnderMouse.Y, 0);
+//        circleCursor.transform.position = cursorPosition;
+//    } else { circleCursor.SetActive(false); }
+//}
 
